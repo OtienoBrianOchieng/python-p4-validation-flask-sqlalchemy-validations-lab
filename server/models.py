3fbf38ad3f6b489502069a7e1d7b3a03b99987a6
root <root@DESKTOP-UEO62DE.localdomain>
2024-01-14 14:31:39 +0300

    Complete validates

--- server/models.py
+++ server/models.py
@@ -12,6 +12,16 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError ('Author must have a name')
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and len(phone_number) != 10:
+            raise ValueError('Not a valid phone number')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +37,28 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError('You must include a title')
+        return title 
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError ('Less than the word count')
+        return content
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError('Post summary must be at most 250 words')
+        return summary
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ('Fiction', 'Non-fiction'):
+            raise ValueError ('Category must be fiction of non-fiction')
+        return category
+    
 
 
     def __repr__(self):
